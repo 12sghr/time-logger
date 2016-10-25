803851f452009656a5040f425142943c9e9d60c7
杉原 <shinta_16n1200325@nnn.ed.jp>
2016-10-25 15:17:35 +0900

    Enable to show data from DB on mainPage

--- routes/mainPage.js
+++ routes/mainPage.js
@@ -1,10 +1,39 @@
 var express = require('express');
+var http = require('http');
 var request = require('request');
 
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('mainPage.ejs');
+  var url = 'http://localhost:9090/mainPage';
+  (() => {
+    var promise = new Promise((resolve) => {
+      http.get(url, function(res){
+        var body = '';
+        res.setEncoding('utf8');
+
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+
+        res.on('end', function(res){
+          ret = JSON.parse(body);
+          //console.log(ret);
+          result = ret;
+          console.log(result);
+          resolve(result);
+        });
+      }).on('error', function(e){
+        console.log(e.message); //エラー時
+      });
+    });
+    return promise;
+  })().then((result) => {
+    console.log(result);
+    res.render('mainPage.ejs', {
+      result,
+    });
+  });
 });
 
 router.post('/', (req, res) => {
@@ -15,6 +44,7 @@ router.post('/', (req, res) => {
   };
   request.post(options, (error, response, body) => {
     console.log(response.body.title);
+    res.redirect('/mainPage');
   });
 });
 
